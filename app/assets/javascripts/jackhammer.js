667e6842f763c5085faf49b87b2614b38f3b60dc
adolpheM <adojoe25@gmail.com>
2017-02-23 12:14:27 +0200

    changed the jackhammer to change color on every click

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -8,12 +8,25 @@
 
 
 var click = 1
+var colors = ["blue", "purple", "red", "green", "yellow"]
 
-function jackhammer(){
-  if (click === 5){
-    alert("You win");
-  } else {
-    console.log("keep trying");
+function changeColor(box){
+  if (click === 1){
+    box.style["background-color"] = colors[0];
+    click++;
+  } else if (click === 2) {
+    box.style["background-color"] = colors[1];
+    click++;
+  }else if (click ===3){
+    box.style["background-color"] = colors[2];
+    click++;
+  }else if (click === 4) {
+    box.style["background-color"] = colors[3]
+    click++;
+  }else if (click === 5) {
+    box.style["background-color"] = colors[4];
+    alert("you win");
     click ++;
   }
+
 }
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,18 +1,18 @@
-var boxOneClick = 0
-var boxTwoClick = 0
-var boxThreeClick = 0
+// var boxOneClick = 0
+// var boxTwoClick = 0
+// var boxThreeClick = 0
 
-function clickAllBox(boxClick){
-  if ((boxOneClick === 1) && (boxTwoClick === 1)  && (boxThreeClick === 0)){
-    alert ("you win");
-  }else if (boxClick === boxOneClick){
-    console.log("Try Again");
-    boxOneClick++;
-  }else if (boxClick === boxTwoClick){
-    console.log("Try Again");
-    boxTwoClick++;
-  }else if (boxClick === boxThreeClick){
-    console.log("Try Again");
-    boxThreeClick++;
-  }
-}
+// function clickAllBox(boxClick){
+//   if ((boxOneClick === 1) && (boxTwoClick === 1)  && (boxThreeClick === 0)){
+//     alert ("you win");
+//   }else if (boxClick === boxOneClick){
+//     console.log("Try Again");
+//     boxOneClick++;
+//   }else if (boxClick === boxTwoClick){
+//     console.log("Try Again");
+//     boxTwoClick++;
+//   }else if (boxClick === boxThreeClick){
+//     console.log("Try Again");
+//     boxThreeClick++;
+//   }
+// }
